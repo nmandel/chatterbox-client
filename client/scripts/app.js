457a9410c96abbc1826fa214569dcb1b7722d8e6
nmandel <nrmandel@ucdavis.edu>
2014-11-10 15:50:36 -0800

    adds user input escaping

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,22 @@
 // YOUR CODE HERE:
+//
+var escaper = function(message) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(message));
+  // console.log(document.createTextNode(message).val());
+  console.log(div);
+  return div.innerHTML;
+};
+
+var messageDisplayer = function(messages) {
+  $('.messages').html('');
+  for (var i = 0; i < messages.results.length; i++) {
+    var name = messages.results[i].username;
+    var msg = messages.results[i].text;
+    // $('.messages').append(escaper(name));
+    $('.messages').append('<p>' + escaper(name) + ': ' + escaper(msg) + '</p>');
+  }
+};
 
 var getMessages = function() {
   $.ajax({
@@ -7,7 +25,7 @@ var getMessages = function() {
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
-      messageDisplayer(data)
+      messageDisplayer(data);
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -16,16 +34,9 @@ var getMessages = function() {
   });
 };
 
-var messageDisplayer = function(messages) {
-  $('.messages').html('');
-
-  for (var i = 0; i < messages.results.length; i++) {
-    $('.messages').append('<p>' +messages.results[i].username + ": " + messages.results[i].text + '</p>');
-  }
-};
 
 getMessages();
-setInterval(getMessages, 1000);
+//setInterval(getMessages, 1000);
 
 
 
