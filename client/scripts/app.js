cdf1924c264f861e745f20a1199ed262ca6f77fa
nmandel <nrmandel@ucdavis.edu>
2014-11-10 15:05:06 -0800

    Displayes messages and auto refreshes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,31 @@
 // YOUR CODE HERE:
 
+var getMessages = function() {
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      messageDisplayer(data)
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message');
+    }
+  });
+};
+
+var messageDisplayer = function(messages) {
+  $('.messages').html('');
+
+  for (var i = 0; i < messages.results.length; i++) {
+    $('.messages').append('<p>' +messages.results[i].username + ": " + messages.results[i].text + '</p>');
+  }
+};
+
+getMessages();
+setInterval(getMessages, 1000);
+
+
+
