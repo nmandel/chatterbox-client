7c22070928931ca9e09cc5d70352e6240bc37e31
nmandel <nrmandel@ucdavis.edu>
2014-11-10 17:17:59 -0800

    Send and sort most recent messages from server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,7 @@
 // YOUR CODE HERE:
-//
 var escaper = function(message) {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(message));
-  // console.log(document.createTextNode(message).val());
-  console.log(div);
   return div.innerHTML;
 };
 
@@ -13,7 +10,6 @@ var messageDisplayer = function(messages) {
   for (var i = 0; i < messages.results.length; i++) {
     var name = messages.results[i].username;
     var msg = messages.results[i].text;
-    // $('.messages').append(escaper(name));
     $('.messages').append('<p>' + escaper(name) + ': ' + escaper(msg) + '</p>');
   }
 };
@@ -24,7 +20,11 @@ var getMessages = function() {
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
+    data: {
+      order: "-createdAt"
+    },
     success: function (data) {
+      // console.log(data);
       messageDisplayer(data);
     },
     error: function (data) {
@@ -34,9 +34,38 @@ var getMessages = function() {
   });
 };
 
+var sendMessage = function(message) {
+  var msgObj = {
+    'username': window.location.search.split("=")[1],
+    'text': message
+  };
+  // console.log(JSON.stringify(msgObj));
+  $.ajax({
+    // always use this url
+    //'order="-createdAt"
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(msgObj),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+$(document).ready(function() {
+  $('button').click(function() {
+    sendMessage($('input').val());
+    // console.log($('input').val());
+  })
+});
 
 getMessages();
-//setInterval(getMessages, 1000);
+setInterval(getMessages, 1000);
 
 
 
